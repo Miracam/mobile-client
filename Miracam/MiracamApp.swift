4a7bfbd5130562b6f41b17612c213ec5d356bc72
JunyaoC <junyaoxiandingchan@gmail.com>
2024-11-15 21:18:33 +0700

    Initial Commit

---
+++ Miracam/MiracamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiracamApp.swift
+//  Miracam
+//
+//  Created by Junyao Chan on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MiracamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiracamTests/MiracamTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiracamTests.swift
+//  MiracamTests
+//
+//  Created by Junyao Chan on 15/11/2024.
+//
+
+import Testing
+@testable import Miracam
+
+struct MiracamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
